28000ec66e5e2816fb349b19f9de56299e5d2d81
Shirogane Dai <dai46hana@gmail.com>
2026-06-29 12:07:45 +0200

    Include window decorations in Windows build

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2,6 +2,8 @@ mod discord_rpc;
 
 use discord_rpc::DiscordState;
 
+use tauri::Manager;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
@@ -13,6 +15,17 @@ pub fn run() {
             discord_rpc::update_discord_presence,
             discord_rpc::disconnect_discord,
         ])
+        .setup(|app| {
+            let window = app.get_webview_window("main").unwrap();
+
+            #[cfg(target_os = "windows")]
+            window.set_decorations(true)?;
+
+            #[cfg(not(target_os = "windows"))]
+            window.set_decorations(false)?;
+
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
