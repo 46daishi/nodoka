5cf0cf2e1c79be16ef13755107cdc692c1ba85f9
Shirogane Dai <dai46hana@gmail.com>
2026-07-17 08:59:01 +0200

    Test bugfix for sound delays after long app usage

--- src/lib/sound.js
+++ src/lib/sound.js
@@ -10,8 +10,10 @@ const _ctx = new (window.AudioContext || window.webkitAudioContext)();
 const _cache = {};
 
 export async function playSound(src) {
-  console.log("Playing sound:", src);
   try {
+    if (_ctx.state === "suspended") {
+      await _ctx.resume();
+    }
     if (!_cache[src]) {
       const res = await fetch(src);
       const buf = await res.arrayBuffer();
@@ -21,6 +23,9 @@ export async function playSound(src) {
     source.buffer = _cache[src];
     source.connect(_ctx.destination);
     source.start(0);
+    source.onended = () => {
+      source.disconnect();
+    };
   } catch (e) {
     console.error("SOUND ERROR:", e);
   }
